beb601b1b48c6ae9cc61e216cfd31d8c52d29f74
futurekind <f.trinler@gmail.com>
2017-03-13 22:39:15 +0100

    Adding game logic

---
+++ src/lib/game.js
@@ -0,0 +1,86 @@
+const makeRow = (row, size) => {
+    if(row.length < size) {
+        return makeRow([
+            ...row,
+            true
+        ], size)
+    } else {
+        return row
+    }
+}
+
+const makeBoard = (board, size) => {
+    if(board.length < size) {
+        return makeBoard([
+            ...board,
+            makeRow([], size)
+        ], size)
+    } else {
+        return board;
+    }     
+}
+
+const _getField = (board, props) => {
+    const { row, col } = props;
+
+    if(
+        typeof props !== 'object' &&
+        !col && !row
+    ) throw new Error(`No correct props parameter is given.`)
+
+    if(typeof col !== 'number' || typeof row !== 'number')
+        throw new Error(`Please provide numbers for row and col`)
+
+    return board[row] 
+        ? board[row][col]
+        : undefined
+}
+
+const updateBoard = (board, row, col) => {
+    const field = _getField(board, {
+        row, col
+    })
+
+    if(field === undefined) return board;
+
+    return board.reduce((prevBoard, boardRow, index) => {
+        if(index === row) {
+            return [
+                ...prevBoard,
+                [
+                    ...boardRow.slice(0, col),
+                    !field,
+                    ...boardRow.slice(col + 1)
+                ]
+            ]
+        }
+        return [
+            ...prevBoard,
+            boardRow
+        ]
+    }, [])
+}
+
+export const createGame = (boardSize =  5) => {
+    let board = makeBoard([], boardSize);
+
+    const toggleField = props => {
+        const { row, col } = props;
+
+        board = updateBoard(board, row, col)
+        board = updateBoard(board, row - 1, col)
+        board = updateBoard(board, row + 1, col)
+        board = updateBoard(board, row, col - 1)
+        board = updateBoard(board, row, col + 1)
+    }
+
+    return {
+        getBoard: () => board,
+        getField: props => _getField(board, props),
+        toggleField,
+        ...process.env.NODE_ENV === 'test' 
+            && { TEST: {
+                boardSize
+            } }
+    }
+}
---
+++ src/lib/game.test.js
@@ -0,0 +1,154 @@
+import { createGame } from './game';
+
+describe('Game', () => {
+
+    describe('constructor', () => {
+        
+        it('it uses 5 as default value', () => {
+            const game = createGame();
+            expect(game.TEST.boardSize).toBe(5)
+        })
+
+        it('sets size as parameter', () => {
+            const game = createGame(7);
+            expect(game.TEST.boardSize).toBe(7)
+        })
+
+    })
+
+    describe('getBoard()', () => {
+        let game;
+
+        beforeAll(() => {
+            game = createGame();
+        })
+
+        it('returns a matrix', () => {
+            const board = game.getBoard()
+            
+            expect(board.length).toBe(5)
+            expect(board[0].length).toBe(5)
+            expect(board[1].length).toBe(5)
+            expect(board[2].length).toBe(5)
+            expect(board[3].length).toBe(5)
+            expect(board[4].length).toBe(5)
+        })
+    })
+
+    describe('getField()',  () => {
+        let game;
+
+        beforeAll(() => {
+            game = createGame(3)
+        })
+
+        it('throws errors', () => {
+            expect(() => {
+                game.getField(3)
+                game.getField('3')
+                game.getField({
+                    row: 3,
+                    col: '3'
+                })
+            }).toThrow()
+        })
+
+        it('returns correct value', () => {
+            expect(
+                game.getField({
+                    row: 1,
+                    col: 1
+                })
+            ).toBe(true)
+        })
+
+        it('returns undefined when no row or col matches', () => {
+            expect(
+                game.getField({
+                    row: 3,
+                    col: 1
+                })
+            ).toBe(undefined)
+
+            expect(
+                game.getField({
+                    row: 3,
+                    col: -1
+                })
+            ).toBe(undefined)
+        })
+
+    })
+
+    describe('toggleField()', () => {
+        let game;
+
+        beforeEach(() => {
+            game = createGame(3)
+        })
+
+        it('toggles clicked field', () => {
+            game.toggleField({
+                row: 1,
+                col: 1
+            })
+
+            expect(game.getField({
+                row: 1, col: 1
+            })).toBe(false)
+        })
+
+        it('also toggles neighbours', () => {
+            const newBoard = game.toggleField({
+                row: 1,
+                col: 1
+            })
+
+            expect(game.getBoard()).toEqual([
+                [true, false, true],
+                [false, false, false],
+                [true, false, true],
+            ])
+        })
+
+        it('also toggles neighbours part 2', () => {
+            const newBoard = game.toggleField({
+                row: 0,
+                col: 0
+            })
+
+            expect(game.getBoard()).toEqual([
+                [false, false, true],
+                [false, true, true],
+                [true, true, true],
+            ])
+        })
+
+        it('also toggles neighbours part 3', () => {
+            const newBoard = game.toggleField({
+                row: 0,
+                col: 2
+            })
+
+            expect(game.getBoard()).toEqual([
+                [true, false, false],
+                [true, true, false],
+                [true, true, true],
+            ])
+        })
+
+        it('also toggles neighbours part 4', () => {
+            const newBoard = game.toggleField({
+                row: 1,
+                col: 0
+            })
+
+            expect(game.getBoard()).toEqual([
+                [false, true, true],
+                [false, false, true],
+                [false, true, true],
+            ])
+        })
+    })
+
+})
